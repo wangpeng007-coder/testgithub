0591ceb6c7294a559e42a277e392fa5f716f418f
wangpeng007-coder <2016191816@qq.com>
2019-09-23 03:35:38 -0400

    modified test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: wangpeng 
+	#	 Email		: wp_21859@163.com 
+	#	 Created	: Mon 23 Sep 2019 03:32:37 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world!\n");
+    return 0;
+}
